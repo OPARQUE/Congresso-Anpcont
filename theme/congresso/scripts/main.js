5cc1c6c3de10e835806cb4aa51911a9d6468b30a
philipes <caiosouza@DESKTOP-GQ34J01.oparque.art.br>
2016-01-11 15:37:23 -0200

    Primeiro commit.
    Primeiro commit.

---
+++ theme/congresso/scripts/main.js
@@ -0,0 +1,91 @@
+﻿"use strict";
+
+$(function () {
+    /*----------------------------------------------------------------------------*/
+    /* Slider */
+    /*----------------------------------------------------------------------------*/
+
+    $(".slider").slick();
+
+    /*----------------------------------------------------------------------------*/
+    /* Menu */
+    /*----------------------------------------------------------------------------*/
+
+    var SHOW_CLASS = "show";
+
+    /* Botão de menu */
+    var menu = $("header menu");
+    var menuButton = $("header button");
+
+    menuButton.click(function (event) {
+        event.stopPropagation();
+
+        menu.toggleClass(SHOW_CLASS);
+        menuButton.toggleClass("close");
+    });
+
+    /* Submenus */
+    var submenus = $("menu ol ol");
+    var submenusButton = $("menu ol h1");
+
+    submenusButton.click(function (event) {
+        event.stopPropagation();
+
+        var submenu = $(this).siblings("ol");
+
+        if (submenu.hasClass(SHOW_CLASS)) {
+            submenu.removeClass(SHOW_CLASS);
+            return;
+        }
+
+        submenus.removeClass(SHOW_CLASS);
+        submenu.addClass(SHOW_CLASS);
+    });
+
+    /* Fecha menu ao clicar fora dele */
+    $('html').click(function () {
+        menuButton.removeClass("close");
+        menu.removeClass(SHOW_CLASS);
+        submenus.removeClass(SHOW_CLASS);
+    });
+
+    $("menu ol, menu ol ol li, .options").click(function (event) {
+        event.stopPropagation();
+    });
+
+    /*----------------------------------------------------------------------------*/
+    /* Tabs */
+    /*----------------------------------------------------------------------------*/
+
+    var SELECTED_CLASS = "selected";
+
+    $.each($(".folder"), function () {
+        var tabs = $(".tab > ol > li", $(this));
+        var content = $(".content > li", $(this));
+
+        /* Expande tudo*/
+        function expandAll() {
+            $(tabs).removeClass(SELECTED_CLASS);
+            $(tabs.get(0)).addClass(SELECTED_CLASS);
+
+            content.addClass(SELECTED_CLASS);
+        }
+
+        $("> h1 .more", $(this)).click(expandAll);
+
+        tabs.click(function () {
+            tabs.removeClass(SELECTED_CLASS);
+            $(this).addClass(SELECTED_CLASS);
+
+            var index = $(this).index();
+            
+            if (index === 0) {
+                expandAll();
+                return;
+            }
+
+            content.removeClass(SELECTED_CLASS);
+            $(content.get(index)).addClass(SELECTED_CLASS);
+        });
+    });
+});
